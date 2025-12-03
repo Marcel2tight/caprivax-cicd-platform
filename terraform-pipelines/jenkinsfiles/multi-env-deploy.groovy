a2a494c7c64f855725247d6838aae0710e01fc2a
Marcel Owhonda <owhondamarcelprince@gmail.com>
2025-12-02 21:40:34 -0800

    feat: synchronize multi-env pipeline script

---
+++ terraform-pipelines/jenkinsfiles/multi-env-deploy.groovy
@@ -0,0 +1,61 @@
+pipeline {
+    agent any
+
+    parameters {
+        choice(name: 'ENVIRONMENT', choices: ['dev', 'staging', 'prod'], description: 'Select target environment')
+        booleanParam(name: 'AUTO_APPROVE', defaultValue: false, description: 'Skip manual approval')
+        booleanParam(name: 'DESTROY', defaultValue: false, description: 'Destroy infrastructure')
+    }
+
+    environment {
+        GIT_CREDENTIALS_ID = 'github-credentials'
+        TF_WORKING_DIR = "jenkins-infrastructure/environments/${params.ENVIRONMENT}"
+    }
+
+    stages {
+        stage('Checkout Code') { steps { checkout scm } }
+
+        stage('Terraform Init') {
+            steps {
+                script {
+                    dir(TF_WORKING_DIR) { sh 'terraform init' }
+                }
+            }
+        }
+
+        stage('Terraform Plan') {
+            steps {
+                script {
+                    dir(TF_WORKING_DIR) {
+                        def varFile = "${params.ENVIRONMENT}.auto.tfvars"
+                        if (params.DESTROY) {
+                            sh "terraform plan -destroy -var-file=\"${varFile}\" -out=tfplan"
+                        } else {
+                            sh "terraform plan -var-file=\"${varFile}\" -out=tfplan"
+                        }
+                    }
+                }
+            }
+        }
+
+        stage('Approval') {
+            when { expression { return !params.AUTO_APPROVE } }
+            steps {
+                script {
+                    input message: "Approve ${params.ENVIRONMENT} deployment?", ok: "Yes"
+                }
+            }
+        }
+
+        stage('Terraform Apply') {
+            steps {
+                script {
+                    dir(TF_WORKING_DIR) {
+                        def cmd = params.DESTROY ? "apply -destroy" : "apply"
+                        sh "terraform ${cmd} -auto-approve tfplan"
+                    }
+                }
+            }
+        }
+    }
+}
